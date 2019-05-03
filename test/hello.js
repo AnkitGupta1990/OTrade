7dff0e868f1fae9fc3784e5e34f6a1d4459dac50
Ankit Gupta <ankit.gupta@a3logics.in>
2019-05-03 15:19:40 +0530

    test

--- test/hello.js
+++ test/hello.js
@@ -1,12 +1,18 @@
 exports.handler = function(event, context, callback) {
 console.log("writing file");
 const fs = require('fs');
-fs.writeFile("/tmp/test", "Hey there!", function(err) {
+fs.writeFile("/tmp/test", "Hey there! NOW!!!", function(err) {
     if(err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
+
+fs.readFile('/tmp/test', 'utf8', function(err, contents) {
+    console.log("reading data from file " + contents);
+});
+ 
+console.log('after calling readFile');
 });
   callback(null, {
     statusCode: 200,
