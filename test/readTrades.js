ed919d02eb018d233d7873241ba229e5671f5ed4
Ankit Gupta <ankit.gupta@a3logics.in>
2019-05-03 15:47:12 +0530

    test

--- test/readTrades.js
+++ test/readTrades.js
@@ -1,6 +1,9 @@
 exports.handler = function(event, context, callback) {
 var contentsdata = '';
 const fs = require('fs');
+fs.exists("/tmp/test", function(fileok){
+console.log("fileok " + fileok); 
+});
 fs.readFile('/tmp/test', 'utf8', function(err, contents) {
 console.log("data 0 " + contents); 
 contentsdata = contents;
