4a81fe6376b1b6bebc4f97433f8044fc60db5b22
Ankit Gupta <ankit.gupta@a3logics.in>
2019-08-06 17:25:52 +0530

    test

--- test/clearTrades.js
+++ test/clearTrades.js
@@ -1,20 +1,54 @@
 exports.handler = function(event, context, callback) {
-const fs = require('fs');
-var error = 'SUCCESS';
-fs.writeFile("/tmp/test", "CLEAR", function(err) {
-    if(err) {
-	error = err;
-        return console.log(err);
-    }
-});
-var contentsdata = '';
-fs.readFile('/tmp/test', 'utf8', function(err, contents) {
-console.log("data 0 " + contents); 
-contentsdata = contents;
-console.log("data " + contentsdata); 
-});
-  callback(null, {
-    statusCode: 200,
-    body: contentsdata
-  });
+	const https = require('https');
+	getLiveDataPromise();
+  	callback(null, {
+    	statusCode: 200,
+    	body: "SUCCESS"
+  	});
 };
+
+async function getLiveDataPromise() {
+  return new Promise((resolve, reject) => {
+    var options = {
+        host: "https://www.nseindia.com",
+        port: 80,
+        path: "live_market/dynaContent/live_watch/get_quote/ajaxFOGetQuoteJSON.jsp?underlying=YESBANK&instrument=OPTSTK&expiry=29AUG2019&type=CE&strike=86.00",
+        method: "GET",
+        headers: {
+            "Referer": "https://www.nseindia.com/",
+            "User-Agent" : "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)"
+        }
+    };
+
+    const req = https.request(options, (response) => {
+      let chunks_of_data = [];
+
+      response.on('data', (fragments) => {
+        chunks_of_data.push(fragments);
+      });
+
+      response.on('end', () => {
+        let response_body = Buffer.concat(chunks_of_data);
+        resolve(response_body.toString());
+      });
+
+      response.on('error', (error) => {
+        reject(error);
+      });
+    });
+    req.end();
+  });
+}
+
+async function getLiveData() {
+  try {
+    let http_promise = getLiveDataPromise();
+    let response_body = await http_promise;
+    console.log(response_body);
+    return response_body;
+  }
+  catch(error) {
+    console.log("Error :  " + error.message);
+    throw error
+  }
+}
